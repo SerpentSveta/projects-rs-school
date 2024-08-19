c6f043c9160c1b1a6812aac098f212212d3cc803
SerpentSveta <sveta.aleksandrova@gmail.com>
2024-08-19 20:49:05 +0300

    feat: add burger-menu Our Pets

--- shelter-part3/src/js/menu.js
+++ shelter-part3/src/js/menu.js
@@ -1,50 +1,46 @@
 const header = document.querySelector('.header');
-const bodyWrapper = document.querySelector('.body-wrapper')
+const bodyWrapper = document.querySelector('.body-wrapper');
 
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('burger-btn').addEventListener('click', function () {
+        header.classList.toggle('open');
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('burger-btn').addEventListener('click', function() {
-      header.classList.toggle('open');
-      
         if (header.classList.contains('open')) {
-    bodyWrapper.classList.add('shading');
+            bodyWrapper.classList.add('shading');
         } else {
-    bodyWrapper.classList.remove('shading');
+            bodyWrapper.classList.remove('shading');
         }
     })
 })
 
 
-window.onload = function() {
-addLinkMenuClickHandler();
+window.onload = function () {
+    addLinkMenuClickHandler();
 }
 
 const addLinkMenuClickHandler = () => {
     document.querySelector('.nav__list').addEventListener('click', (e) => {
-    closeMobileMenu();
+        closeMobileMenu();
         if (header.classList.contains('open')) {
             bodyWrapper.classList.add('shading');
         } else {
             bodyWrapper.classList.remove('shading');
-        }    
+        }
     })
 }
 
 const closeMobileMenu = () => {
     let navItem = document.querySelectorAll('.nav__item .link');
     navItem.forEach(link => {
-    header.classList.remove('open');
+        header.classList.remove('open');
     })
 }
 
-bodyWrapper.addEventListener('click', function() {
+bodyWrapper.addEventListener('click', function () {
     header.classList.toggle('open');
-     bodyWrapper.classList.remove('shading');
+    bodyWrapper.classList.remove('shading');
 })
 
-bodyWrapper.querySelector('header').addEventListener('click', function(e) {
-e.stopPropagation();
+bodyWrapper.querySelector('header').addEventListener('click', function (e) {
+    e.stopPropagation();
 })
-
-
-
