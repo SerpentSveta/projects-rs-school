209fbae0f5443dcb8b8fa75533a6c9124bef50a3
SerpentSveta <sveta.aleksandrova@gmail.com>
2024-09-25 22:19:16 +0300

    feat: show img

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,9 +1,10 @@
 const search = document.querySelector('.form-control');
 const searchBtn = document.querySelector('.btn');
 const errors = document.querySelector('.errors');
+const columns = document.querySelectorAll('.col-md-4');
 
 
-const url = "https://api.unsplash.com/photos/?client_id=CVaahuYwRuXc5bwgyNnK6d5bRpvBjn-IH51-sSIGaqs&per_page=30";
+const url = "https://api.unsplash.com/photos/?client_id=CVaahuYwRuXc5bwgyNnK6d5bRpvBjn-IH51-sSIGaqs&orientation=portrait";
 const imagesURL = [];
 
 
@@ -27,4 +28,21 @@ const showData = () => {
     if (imagesURL == 0) {
         errors.innerHTML = `Try to come back later`;
     }
+
+    createImg();
+}
+
+const createImg = () => {
+    imagesURL.forEach((url, index) => {
+
+        if (index < columns.length) {
+            const img = document.createElement("img");
+            img.classList.add("w-100", "shadow-1-strong", "rounded", "mb-4");
+            img.src = url;
+            img.alt = `image`;
+
+            columns[index].append(img);
+
+        }
+    })
 }
